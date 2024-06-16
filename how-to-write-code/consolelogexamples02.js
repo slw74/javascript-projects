860643855274dc15ce4b1222a1b0a60833b94360
slw74 <shilanda.woolridge@gmail.com>
2024-06-15 23:34:55 -0500

    experiment with newlines and tabs

--- how-to-write-code/consolelogexamples02.js
+++ how-to-write-code/consolelogexamples02.js
@@ -1,3 +1,8 @@
 console.log("Some Programming Languages:");
 
 console.log("Python\nJavaScript\nJava\nC#\nSwift");
+
+console.log("\nAn experiment with newlines and tabs:");
+
+//experiments with tabs and newlines start here
+console.log("\n\tPython\n\t\tJavaScript\n\t\t\tJava\n\t\t\t\tC#\n\t\t\t\t\tSwift");
