00ab2e4bf89f6f71a932f34c61fc85a11bfa0f47
slw74 <shilanda.woolridge@gmail.com>
2024-06-21 20:43:55 -0500

    ch 8 exercises arrays

--- arrays/exercises/part-five-arrays.js
+++ arrays/exercises/part-five-arrays.js
@@ -2,15 +2,29 @@ let str = 'In space, no one can hear you code.';
 let arr = ['B', 'n', 'n', 5];
 
 //1) Use the split method on the string to identify the purpose of the parameter inside the ().
-console.log(str.split());
-console.log(str.split('e'));
-console.log(str.split(' '));
-console.log(str.split(''));
+console.log(str.split());       //there is no delimiter to determine how the string is broken apart  
+console.log(str.split('e'));    //split the string at each letter e
+console.log(str.split(' '));    //split the string at each space
+console.log(str.split(''));     //split the string at each character
 
 //2) Use the join method on the array to identify the purpose of the parameter inside the ().
+newString1 = arr.join("+");     //the "+" will be the connector that glues the array elements together
+console.log(newString1); 
 
+newString2 = arr.join("");      //the "" character will be the connector
+console.log(newString2);
+
+newString3 = arr.join("_");     //the "_" underscore will be the connector
+console.log(newString3);
+
+newString4 = arr.join(" ");     //the " "   space will be the connector
+console.log(newString4);
+ 
 
 //3) Do split or join change the original string/array?
+//DEBUGGER'S NOTE - Yes and no.  While they don't change the items within the original string/array they do change how it is presented after the code is run.
 
 //4) We can take a comma-separated string and convert it into a modifiable array. Try it! Alphabetize the cargoHold string, and then combine the contents into a new string.
 let cargoHold = "water,space suits,food,plasma sword,batteries";
+
+console.log(cargoHold.split(',').sort().join(','));
--- arrays/exercises/part-six-arrays.js
+++ arrays/exercises/part-six-arrays.js
@@ -6,10 +6,33 @@ let element2 = ['helium', 'He', 4.003];
 let element26 = ['iron', 'Fe', 55.85];
 
 //2) Define the array 'table', and use 'push' to add each of the element arrays to it. Print 'table' to see its structure.
+//DEBUGGER'S NOTE - We were not shown how to use push to create a two dimensional array from scratch so i'm going to define one using the code example at the bottom of the multi-dimensional arrays reading.
+
+let table = [
+    ['hydrogen', 'H', 1.008],
+    ['helium', 'He', 4.003],
+    ['iron', 'Fe', 55.85],
+];
 
 
 //3) Use bracket notation to examine the difference between printing 'table' with one index vs. two indices (table[][]).
 
+console.log("\nprinting the result of a whole table variable");
+console.log(table);
+
+console.log("\nprinting the result of individual bracket notations from within a multi-dimensional array");
+console.log(table[0][2]);
+console.log(table[1][1]);
+console.log(table[2][2]);
+
+
 //4) Using bracket notation and the table array, print the mass of element1, the name for element 2 and the symbol for element26.
+console.log("\nthe mass of element 1");
+console.log(table[0][2]);
+console.log("the name for element 2");
+console.log(table[1][0]);
+console.log("the name symbol for the 26th element on the periodic table");
+console.log(table[2][1]);
 
 //5) 'table' is an example of a 2-dimensional array. The first “level” contains the element arrays, and the second level holds the name/symbol/mass values. Experiment! Create a 3-dimensional array and print out one entry from each level in the array.
+//DEBUGGER'S NOTE - Didn't we just do this?  Technically the table array with the elements has three lines of arrays in it.
